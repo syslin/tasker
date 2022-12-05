3c6e443acf1b3684d381c0327e6b664bf97c6cf0
Syslin <saneemastha@gmail.com>
2022-12-05 19:58:40 +0545

    stimulu reflex installed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,9 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "controllers/application"
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+import StimulusReflex from 'stimulus_reflex'
+import consumer from '../channels/consumer'
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
@@ -9,3 +12,10 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+const application = Application.start()
+const context = require.context('controllers', true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
+application.consumer = consumer
+StimulusReflex.initialize(application, { consumer })
+
